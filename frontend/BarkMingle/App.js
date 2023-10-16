5625af389c4e9dd99a23961173e073f495c12822
JacquelineMG <j.gilchrist@outlook.com>
2023-10-16 11:05:56 -0700

    add react native to project with expo

---
+++ frontend/BarkMingle/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>The triumphant beginnings of Bark/Mingle</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
