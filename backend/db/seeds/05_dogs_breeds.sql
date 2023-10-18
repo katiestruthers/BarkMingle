933ddb28904d86cfcf87f6ed1c245cfdf0200d5f
Katherine Struthers <ktstruthers@gmail.com>
2023-10-17 22:37:43 -0500

    create preliminary seeds

---
+++ backend/db/seeds/05_dogs_breeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO dogs_breeds (dog_id, breed_id)
+VALUES
+  (1, 5),
+  (2, 1),
+  (3, 2),
+  (4, 7),
+  (5, 12);
---
+++ backend/db/seeds/09_matches.sql
@@ -0,0 +1,7 @@
+INSERT INTO matches (user_1_id, user_2_id)
+VALUES
+  (1, 3),
+  (2, 4),
+  (3, 5),
+  (4, 1),
+  (5, 2);
