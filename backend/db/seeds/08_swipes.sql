9af9399d0e3221c4b1431caf0922d25fcb967d7b
Katherine Struthers <ktstruthers@gmail.com>
2023-10-19 13:40:55 -0500

    change likes table to swipes table to account for rejections

---
+++ backend/db/seeds/08_swipes.sql
@@ -0,0 +1,14 @@
+INSERT INTO swipes (swiped_by_user_id, swiped_user_id, is_liked)
+VALUES
+  (1, 3, true),
+  (3, 1, true),
+  (2, 4, true),
+  (4, 2, true),
+  (3, 5, true),
+  (5, 3, true),
+  (4, 1, true),
+  (1, 4, true),
+  (5, 2, true),
+  (2, 5, true),
+  (4, 3, false),
+  (5, 4, false);
