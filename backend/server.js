6b889f6efe82134e2074b796f77245097e87392e
Soyoung <ksyoung3424@gmail.com>
2023-10-20 06:34:57 +0000

    Add signup, signup/dog, login POST

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,32 @@ const App = Express();
 const BodyParser = require('body-parser');
 const PORT = 8080;
 
+
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(BodyParser.json());
-App.use(Express.static('public'));
 
-// Sample GET route
-App.get('/api/data', (req, res) => res.json({
-  message: "Seems to work!",
+// Breakdown routes
+// const apiRoutes = require("./routes/NAME!!"); // one table
+const userRoutes = require('./routes/signup'); // one table
+
+
+// /api/endpoitns
+App.use("/signup", userRoutes);
+
+// Sample POST route
+App.post('/signup', (req, res) => res.json({
+  message: "SIGNUP to work!",
+  body: req.body
 }));
+
+// curl -d '{"name":"donut"}' -H "Content-Type: application/json" -X POST http://localhost:8080/signup/dog
+
+App.post('/signup/dog', (req, res) => res.json({
+  message: "SIGNUP/DOG work!",
+  body: req.body
+}));
+
 
 App.listen(PORT, () => {
   // eslint-disable-next-line no-console
