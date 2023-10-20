2550e4e30757501e0183d1eded5ce76c7b4e2e06
JacquelineMG <j.gilchrist@outlook.com>
2023-10-19 18:00:14 -0700

    start basic login screen

---
+++ frontend/BarkMingle/screens/LoginScreen.js
@@ -0,0 +1,43 @@
+import { useNavigation } from '@react-navigation/core';
+import React from 'react';
+import { View, Text, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
+import styles from "../styles/loginStyles.js"
+
+const LoginScreen = () => {
+  
+  const navigation = useNavigation();
+
+    return (
+      <View style={styles.container}>
+        <ImageBackground 
+          source={require('../assets/dogbones.jpg')}
+          style={styles.background}>
+  
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Email"
+              placeholderTextColor="#003f5c"
+            />
+          </View>
+  
+          <View style={styles.inputView}>
+            <TextInput
+              style={styles.textInput}
+              placeholder="Password"
+              placeholderTextColor="#003f5c"
+              secureTextEntry={true}
+            />
+          </View>
+  
+          <TouchableOpacity style={styles.loginButton} onPress={() => navigation.navigate("Login")}>
+            <Text style={styles.textInput}> Sign In </Text>
+          </TouchableOpacity>
+        
+        </ImageBackground>
+      </View>
+
+  )
+};
+
+export default LoginScreen
