b596d4fdb213f47106d095adb52bc175ff0038a6
Soyoung <ksyoung3424@gmail.com>
2023-10-21 19:01:39 +0000

    Updated the defalut password to the same hased password for development

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -55,7 +55,6 @@ router.post("/signup", (req, res) => {  // only /signup
 
 
 
-
 // Login route
 router.post("/signin", (req, res) => {
   const { email, password } = req.body;
@@ -94,6 +93,50 @@ router.post("/signin", (req, res) => {
     }
   });
 });
+
+
+
+// Update user profile   LOCATIONS????
+router.put("/:id", (req, res) => {
+  const userId = req.params.id;    // id captured from the url
+  const { first_name, last_name, bio, profile_img } = req.body;
+ 
+  if (!userId) {
+    return res.status(401).json({ message: 'User is not logged in' });
+  }
+
+  if (!first_name || !last_name) {
+    return res.status(400).json({ error: "First and last name required" });
+  }
+
+  // Build the SQL query for updating the user's profile
+  const query = `
+    UPDATE users
+    SET first_name = $1, last_name = $2, bio = $3, profile_img = $4
+    WHERE id = $5
+    RETURNING id, first_name, last_name, bio, profile_img, email
+  `;
+
+  // Execute the query
+  database.query(query, [first_name, last_name, bio, profile_img, userId], (error, result) => {
+    if (error) {
+      console.error("Error updating user profile:", error);
+      return res.status(500).json({ error: "Failed to update user profile" });
+    }
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const updatedUser = result.rows[0];
+
+    res.json({
+      message: "User profile updated successfully",
+      user: updatedUser
+    });
+  });
+});
+
 
 
 module.exports = router;
