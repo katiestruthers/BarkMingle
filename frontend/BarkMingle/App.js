bd44ab7b9367e3e36884d76a3d1883e5ef4e1576
Katherine Struthers <ktstruthers@gmail.com>
2023-10-24 12:16:49 -0500

    set up axios

--- frontend/BarkMingle/App.js
+++ frontend/BarkMingle/App.js
@@ -3,8 +3,18 @@ import StackNavigator from './StackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
 import { AuthProvider } from './hooks/useAuth';
 import { useFonts, Baloo2_400Regular, Baloo2_500Medium, Baloo2_600SemiBold, Baloo2_800ExtraBold } from '@expo-google-fonts/baloo-2';
+import Axios from "axios";
 
 export default function App() {
+  Axios({
+    method: "GET",
+    url: "http://localhost:8080/api/data",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    console.log(res.data.message);
+  });
 
   let [fontsLoaded, fontError] = useFonts({
     Baloo2_400Regular,
