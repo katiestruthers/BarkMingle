295d9ce99b64c3ae03cf376b334cce1a3102980e
carmshito <carmshito@hotmail.com>
2023-10-30 12:48:48 -0400

    fix navBar styling

--- frontend/BarkMingle/components/NavBar.js
+++ frontend/BarkMingle/components/NavBar.js
@@ -2,33 +2,30 @@ import { useNavigation } from "@react-navigation/core";
 import React from "react";
 import { TouchableOpacity, View } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faUser } from '@fortawesome/free-solid-svg-icons/faUser';
-import { faDog } from '@fortawesome/free-solid-svg-icons/faDog';
-import { faPaw } from '@fortawesome/free-solid-svg-icons/faPaw';
+import { faUser } from "@fortawesome/free-solid-svg-icons/faUser";
+import { faDog } from "@fortawesome/free-solid-svg-icons/faDog";
+import { faPaw } from "@fortawesome/free-solid-svg-icons/faPaw";
 import styles from "../styles/navBarStyles";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-
 const NavBar = () => {
-
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={styles.navBar}>  
+    <View style={styles.navBar}>
       <TouchableOpacity onPress={() => navigation.navigate("Matches")}>
-      <FontAwesomeIcon icon={ faPaw } size={50}/>
+        <FontAwesomeIcon icon={faPaw} size={50} />
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.navigate("Home")}>
-      <FontAwesomeIcon icon={ faDog } size={50}/>
+        <FontAwesomeIcon icon={faDog} size={50} />
       </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.navigate("UserProfile")}>
-        <FontAwesomeIcon icon={ faUser } size={50}/>
+        <FontAwesomeIcon icon={faUser} size={50} />
       </TouchableOpacity>
-    </SafeAreaView>
-      
-  )
-}
+    </View>
+  );
+};
 
-export default NavBar
+export default NavBar;
