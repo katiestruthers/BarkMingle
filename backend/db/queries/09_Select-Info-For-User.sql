b83ff2fb95983e2457cab8e08dd6867e58acfe50
Katherine Struthers <ktstruthers@gmail.com>
2023-10-30 16:51:32 -0500

    write query to get user info

---
+++ backend/db/queries/09_Select-Info-For-User.sql
@@ -0,0 +1,23 @@
+SELECT
+  dogs.id AS dog_id,
+  dogs.name AS dog_name,
+  dogs.age AS dog_age, 
+  dogs.is_neutered, 
+  dogs.gender AS dog_gender, 
+  dogs.size AS dog_size, 
+  dogs.img AS dog_img,
+  breeds.name AS breed, 
+  users.id AS user_id,
+  users.first_name, users.last_name,
+  users.bio AS user_bio,
+  users.profile_img
+  
+FROM dogs
+JOIN dogs_breeds
+ON dogs.id = dogs_breeds.dog_id
+JOIN breeds
+ON dogs_breeds.breed_id = breeds.id
+JOIN users
+ON dogs.user_id = users.id
+
+WHERE NOT users.id = 1;
